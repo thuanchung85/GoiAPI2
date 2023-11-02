7305c8849446beccc9da163e6fe463384dfc1f8d
Thuận Chung Lương <thuanchung85@gmail.com>
2023-11-02 11:15:03 +0700

    save

---
+++ Sources/GoiAPI2/Add_AccountsView/Childs View/SheetCreateAccountView.swift
@@ -0,0 +1,79 @@
+import SwiftUI
+import UniformTypeIdentifiers
+import Foundation
+import web3swift
+import Web3Core
+
+
+public struct SheetCreateAccountView: View {
+    
+    @Binding var add_NewAccountName:String
+    @Binding var isShow_SheetEnterWalletName:Bool
+    @Binding var arr_Accounts:[Account_Type]
+    
+    //===INIT==//
+    public init(add_NewAccountName:Binding<String>, isShow_SheetEnterWalletName:Binding<Bool>, arr_Accounts:Binding<[Account_Type]>)  {
+        self._add_NewAccountName = add_NewAccountName
+        self._isShow_SheetEnterWalletName = isShow_SheetEnterWalletName
+        self._arr_Accounts = arr_Accounts
+    }
+    
+    //===BODY===//
+    public var body: some View {
+        VStack(alignment: .center){
+            HStack{
+                Spacer()
+                Text("Create New Wallet")
+                    .font(.custom("Arial Bold", size: 20))
+                    .padding(.top,15)
+                Spacer()
+            }
+            HStack{
+                VStack(alignment: .leading){
+                    Text("Wallet Name")
+                        .font(.custom("Arial ", size: 15))
+                        .padding(.top,15)
+                        .padding(.horizontal,20)
+                    TextField("Enter your wallet name", text: self.$add_NewAccountName)
+                        .frame(height: 60)
+                        .textFieldStyle(PlainTextFieldStyle())
+                        .padding([.horizontal], 4)
+                        .cornerRadius(10)
+                        .overlay(RoundedRectangle(cornerRadius: 16).stroke(Color.gray))
+                        .padding([.horizontal], 20)
+                    
+                    Spacer()
+                    
+                    //nut add account
+                    if(self.add_NewAccountName.isEmpty == false){
+                        Button(action: {
+                            print("Create Account")
+                            //off this sheet
+                            self.isShow_SheetEnterWalletName = false
+                            //tạo account mới
+                            let newAcc = Account_Type(nameWallet: self.add_NewAccountName,
+                                                      addressWallet: "making...", pkey: "making...")
+                            self.arr_Accounts.append(newAcc)
+                            //xoa tên account vì đã tạo xong
+                            self.add_NewAccountName = ""
+                        }) {
+                            HStack{
+                                Text("Create")
+                                    .foregroundColor(Color.white)
+                                    .font(.custom("Arial", size: 20))
+                                    .padding(.horizontal,5)
+                            }
+                            .frame(maxWidth: .infinity, minHeight: 60 ,maxHeight: 60)
+                            .background(Color.green)
+                            .cornerRadius(10)
+                            .padding(.horizontal,20)
+                            .padding(.bottom,50)
+                        }
+                    }
+                }//end VStack
+            }//end HStack
+         }//end VStack
+        
+    }
+    
+}//end struct
