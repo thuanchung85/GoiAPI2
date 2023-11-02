b5c225cb16ab26105a948e7dc04305f447602680
Thuận Chung Lương <thuanchung85@gmail.com>
2023-11-02 13:26:59 +0700

    save

--- Sources/GoiAPI2/Add_AccountsView/Childs View/SheetCreateAccountView.swift
+++ Sources/GoiAPI2/Add_AccountsView/Childs View/SheetCreateAccountView.swift
@@ -13,6 +13,8 @@ public struct SheetCreateAccountView: View {
     
     //biến lưu lại địa chỉ account tạm, khi user ok thì sẽ dùng nó còn không ok thì bỏ
     @State var tempAddress:String = ""
+    @State var isFirstResponder = false
+    
     
     //===INIT==//
     public init(add_NewAccountName:Binding<String>, isShow_SheetEnterWalletName:Binding<Bool>, arr_Accounts:Binding<[Account_Type]>)  {
@@ -37,7 +39,8 @@ public struct SheetCreateAccountView: View {
                         .font(.custom("Arial ", size: 15))
                         .padding(.top,15)
                         .padding(.horizontal,20)
-                    TextField("Enter your wallet name", text: self.$add_NewAccountName)
+                    //TextField("Enter your wallet name", text: self.$add_NewAccountName)
+                    LegacyTextField(text: self.$add_NewAccountName, isFirstResponder: $isFirstResponder)
                         .frame(height: 60)
                         .textFieldStyle(PlainTextFieldStyle())
                         .padding([.horizontal], 4)
@@ -57,6 +60,7 @@ public struct SheetCreateAccountView: View {
                             let newAcc = Account_Type(nameWallet: self.add_NewAccountName,
                                                       addressWallet: self.tempAddress, pkey: "making...")
                             self.arr_Accounts.append(newAcc)
+                            self.isFirstResponder = true
                             //xoa tên account vì đã tạo xong
                             self.add_NewAccountName = ""
                         }) {
@@ -97,4 +101,59 @@ func makeEthereumAddressAccount() -> String
      print(error.localizedDescription)
      }
     return "error no data"
+}
+
+
+struct LegacyTextField: UIViewRepresentable {
+    @Binding public var isFirstResponder: Bool
+    @Binding public var text: String
+
+    public var configuration = { (view: UITextField) in }
+
+    public init(text: Binding<String>, isFirstResponder: Binding<Bool>, configuration: @escaping (UITextField) -> () = { _ in }) {
+        self.configuration = configuration
+        self._text = text
+        self._isFirstResponder = isFirstResponder
+    }
+
+    public func makeUIView(context: Context) -> UITextField {
+        let view = UITextField()
+        view.addTarget(context.coordinator, action: #selector(Coordinator.textViewDidChange), for: .editingChanged)
+        view.delegate = context.coordinator
+        return view
+    }
+
+    public func updateUIView(_ uiView: UITextField, context: Context) {
+        uiView.text = text
+        switch isFirstResponder {
+        case true: uiView.becomeFirstResponder()
+        case false: uiView.resignFirstResponder()
+        }
+    }
+
+    public func makeCoordinator() -> Coordinator {
+        Coordinator($text, isFirstResponder: $isFirstResponder)
+    }
+
+    public class Coordinator: NSObject, UITextFieldDelegate {
+        var text: Binding<String>
+        var isFirstResponder: Binding<Bool>
+
+        init(_ text: Binding<String>, isFirstResponder: Binding<Bool>) {
+            self.text = text
+            self.isFirstResponder = isFirstResponder
+        }
+
+        @objc public func textViewDidChange(_ textField: UITextField) {
+            self.text.wrappedValue = textField.text ?? ""
+        }
+
+        public func textFieldDidBeginEditing(_ textField: UITextField) {
+            self.isFirstResponder.wrappedValue = true
+        }
+
+        public func textFieldDidEndEditing(_ textField: UITextField) {
+            self.isFirstResponder.wrappedValue = false
+        }
+    }
 }
