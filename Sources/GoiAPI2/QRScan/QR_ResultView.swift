5413eb7418d4d978e6d79e784529faaa9d9e3b65
Thuận Chung Lương <thuanchung85@gmail.com>
2023-11-07 08:04:58 +0700

    save

--- Sources/GoiAPI2/QRScan/QR_ResultView.swift
+++ Sources/GoiAPI2/QRScan/QR_ResultView.swift
@@ -27,26 +27,58 @@ public struct QR_ResultView: View {
                 .cornerRadius(10)
                 .overlay(RoundedRectangle(cornerRadius: 16).stroke(Color.gray))
                 .padding([.horizontal], 20)
+            
+            //nếu đia chỉ ví là ethereum
+            if(self.qrResultString.count == 42){
+                //nut save account
+                Button(action: {
+                    print("Save this wallet address")
+                    //save wallet vao máy
+                    
+                    //dismiss
+                    isShow_ScanQRcodeView = false
+                }) {
+                    HStack{
+                        Image(systemName: "square.and.pencil")
+                            .renderingMode(.template)
+                            .foregroundColor(Color.white)
+                        Text("Save this wallet address")
+                            .foregroundColor(Color.white)
+                            .font(.custom("Arial", size: 20))
+                            .padding(.horizontal,5)
+                    }
+                    .frame(maxWidth: .infinity, minHeight: 60 ,maxHeight: 60)
+                    .background(Color.green)
+                    .cornerRadius(10)
+                    .padding(.horizontal,20)
+                }.padding(15)
+            }
+            else{
+                Text("This is NOT ETHEREUM wallet address!")
+                    .foregroundColor(Color.black)
+                    .font(.custom("Arial Bold", size: 20))
+                    .padding(15)
+                //nut save account
+                Button(action: {
+                    print("back this is not wallet address")
+                    isShow_ScanQRcodeView = false
+                }) {
+                    HStack{
+                        Image(systemName: "square.and.pencil")
+                            .renderingMode(.template)
+                            .foregroundColor(Color.white)
+                        Text("Back")
+                            .foregroundColor(Color.white)
+                            .font(.custom("Arial", size: 20))
+                            .padding(.horizontal,5)
+                    }
+                    .frame(maxWidth: .infinity, minHeight: 60 ,maxHeight: 60)
+                    .background(Color.green)
+                    .cornerRadius(10)
+                    .padding(.horizontal,20)
+                }.padding(15)
+            }
             Spacer()
-            //nut save account
-            Button(action: {
-               print("Save this wallet address")
-                isShow_ScanQRcodeView = false
-            }) {
-                HStack{
-                    Image(systemName: "square.and.pencil")
-                        .renderingMode(.template)
-                        .foregroundColor(Color.white)
-                    Text("Save this wallet address")
-                        .foregroundColor(Color.white)
-                        .font(.custom("Arial", size: 20))
-                        .padding(.horizontal,5)
-                }
-                .frame(maxWidth: .infinity, minHeight: 60 ,maxHeight: 60)
-                .background(Color.green)
-                .cornerRadius(10)
-                .padding(.horizontal,20)
-            }.padding(15)
         }
         
     }//end body
