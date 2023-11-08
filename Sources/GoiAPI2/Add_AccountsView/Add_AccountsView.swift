0ad79beb3b56df973c01670bd1e0c498828f63c7
Thuận Chung Lương <thuanchung85@gmail.com>
2023-11-08 10:28:35 +0700

    save

--- Sources/GoiAPI2/Add_AccountsView/Add_AccountsView.swift
+++ Sources/GoiAPI2/Add_AccountsView/Add_AccountsView.swift
@@ -252,7 +252,7 @@ public struct Add_AccountsView: View {
                     {
                         let arrayOfParams = returnString!.components(separatedBy: "+|@|+")
                         print(arrayOfParams)
-                        if(arrayOfParams.count == 3){
+                        if(arrayOfParams.count == 4){
                             let sig = UserDefaults.standard.string(forKey: "signatureOfAccount<->\(arrayOfParams[1])") ?? "no found"
                             let newAcc = Account_Type(nameWallet: arrayOfParams.first!,
                                                       addressWallet: arrayOfParams[1], pkey: arrayOfParams.last!,
